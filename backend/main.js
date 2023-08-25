a84ecd8849e909457ff8026bb8d9e5cd1267cc87
emilvn <emilvnielsen@hotmail.com>
2023-08-25 10:20:08 +0200

    added server functionality

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,15 @@
-console.log("test");
+import express from "express";
+import router from "./router.js";
+import cors from "cors";
+
+const app = express();
+const port = 3333;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, ()=>{
+	console.log(`app  running on http://localhost:${port}`);
+});
+
+

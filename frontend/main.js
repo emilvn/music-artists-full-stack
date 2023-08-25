bbe9b9c854c9c48c95538a1cd3353e58246548a2
emilvn <emilvnielsen@hotmail.com>
2023-08-25 20:10:02 +0200

    added updateArtist

--- frontend/main.js
+++ frontend/main.js
@@ -5,6 +5,7 @@ window.addEventListener("load", main);
 
 const endpoint = "http://localhost:3333";
 let artists;
+let selectedArtist;
 
 async function main(){
 	await getArtists();
@@ -34,6 +35,47 @@ export async function addArtist(artist){
 
 	if(response.ok){
 		artists.push(artist);
+		displayArtists(artists);
+		scrollToTop();
+	}
+}
+
+export function selectArtist(artist){
+	selectedArtist = artist;
+	const form = document.querySelector("#form-update");
+	form.name.value = artist.name;
+	form.birthdate.value = artist.birthdate;
+	form.activeSince.value = artist.activeSince;
+	form.image.value = artist.image;
+	form.genres.value = String(artist.genres);
+	form.labels.value = String(artist.labels);
+	form.roles.value = String(artist.roles);
+	form.website.value = artist.website;
+	form.shortDescription.value = artist.shortDescription;
+	form.scrollIntoView({ behavior: "smooth" });
+}
+
+export async function updateArtist(updatedArtist){
+
+	const response = await fetch(endpoint + "/artists/" + updatedArtist.id, {
+		method: "PUT",
+		headers: {
+			"Content-Type":"application/json"
+		},
+		body: JSON.stringify(updatedArtist)
+	});
+	if(response.ok){
+		const artistToUpdate = artists.find(artist => artist.id === selectedArtist.id);
+		artistToUpdate.name = updatedArtist.name;
+		artistToUpdate.birthdate = updatedArtist.birthdate;
+		artistToUpdate.activeSince = updatedArtist.activeSince;
+		artistToUpdate.image = updatedArtist.image;
+		artistToUpdate.genres = updatedArtist.genres;
+		artistToUpdate.labels = updatedArtist.labels;
+		artistToUpdate.roles = updatedArtist.roles;
+		artistToUpdate.website = updatedArtist.website;
+		artistToUpdate.shortDescription = updatedArtist.shortDescription;
+
 		displayArtists(artists);
 		scrollToTop();
 	}

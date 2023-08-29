4d0e93f29362ff8583d794a6a344c0675c195506
emilvn <emilvnielsen@hotmail.com>
2023-08-29 21:36:07 +0200

    added comments to server

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,31 @@
+/* ========== IMPORTS ========== */
 import express from "express";
-import cors from "cors";
-import {
-	addArtistData,
-	addFavorite,
-	deleteArtist,
-	getArtistsData,
-	getFavoritesData, getSpecificArtist, removeFromFavorites,
-	updateArtistData
-} from "./controller.js";
 import {router} from "./router.js";
+import cors from "cors";
 
+/* ========== EXPRESS ========== */
+// instance of an Express application. //
 const app = express();
+
+// Port for server to listen on //
 const port = 3333;
 
+/* ========== MIDDLEWARE SETUP ========== */
+
+// Parse incoming JSON data. //
 app.use(express.json());
+
+// Enable CORS to allow cross-origin requests. //
 app.use(cors());
 
+/* ========== ROUTER SETUP ========== */
+
+// Use the router defined in "./router.js" //
 app.use("/", router);
 
+/* ========== START SERVER ========== */
+
+// Start the Express server and listen on the defined port. //
 app.listen(port, ()=>{
 	console.log(`app  running on http://localhost:${port}`);
 });

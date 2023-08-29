899fdc7bb64833b04265917638bf5e28f705b453
emilvn <emilvnielsen@hotmail.com>
2023-08-29 10:00:21 +0200

    moved data to data folder

--- backend/controller.js
+++ backend/controller.js
@@ -12,29 +12,29 @@ function writeArtistsToFile(artistsArr, path){
 
 
 export async function getArtistsData(req, res){
-	const artists = await getArtists("artists.json");
+	const artists = await getArtists("data/artists.json");
 	res.json(artists);
 }
 
 export async function getSpecificArtist(req, res){
 	const id = req.params.id;
-	const artists = await getArtists("artists.json");
+	const artists = await getArtists("data/artists.json");
 	res.json(artists.find(artist => artist.id === id));
 }
 
 export async function addArtistData(req, res){
-	const artists = await getArtists("artists.json");
+	const artists = await getArtists("data/artists.json");
 	const newArtist = req.body;
 	newArtist.id = uuidv4();
 	artists.push(newArtist);
-	writeArtistsToFile(artists, "artists.json");
+	writeArtistsToFile(artists, "data/artists.json");
 
 	res.json(artists);
 }
 
 export async function updateArtistData(req, res){
 	const id = req.params.id;
-	const artists = await getArtists("artists.json");
+	const artists = await getArtists("data/artists.json");
 	const artistToUpdate = artists.find(artist => artist.id === id);
 	const body = req.body;
 
@@ -48,40 +48,40 @@ export async function updateArtistData(req, res){
 	artistToUpdate.image = body.image;
 	artistToUpdate.shortDescription = body.shortDescription;
 
-	writeArtistsToFile(artists, "artists.json");
+	writeArtistsToFile(artists, "data/artists.json");
 	res.json(artists);
 }
 
 export async function deleteArtist(req, res){
 	const id = req.params.id;
-	const artists = await getArtists("artists.json");
+	const artists = await getArtists("data/artists.json");
 
 	const updatedArtists = artists.filter(artist => artist.id !== id);
 
-	writeArtistsToFile(updatedArtists, "artists.json");
+	writeArtistsToFile(updatedArtists, "data/artists.json");
 	res.json(updatedArtists);
 }
 
 export async function getFavoritesData(req, res){
-	const favorites = await getArtists("favorites.json");
+	const favorites = await getArtists("data/favorites.json");
 	res.json(favorites);
 }
 
 export async function addFavorite(req, res){
-	const favorites = await getArtists("favorites.json");
+	const favorites = await getArtists("data/favorites.json");
 	const artist = req.body;
 	if(!favorites.find(favorite => favorite.id === artist.id)){
 		favorites.push(artist);
-		writeArtistsToFile(favorites, "favorites.json");
+		writeArtistsToFile(favorites, "data/favorites.json");
 	}
 	res.json(favorites);
 }
 
 export async function removeFromFavorites(req, res){
-	const favorites = await getArtists("favorites.json");
+	const favorites = await getArtists("data/favorites.json");
 	const id = req.params.id;
 	const updatedFavorites = favorites.filter(artist => artist.id !== id);
 
-	writeArtistsToFile(updatedFavorites, "favorites.json");
+	writeArtistsToFile(updatedFavorites, "data/favorites.json");
 	res.json(updatedFavorites);
 }

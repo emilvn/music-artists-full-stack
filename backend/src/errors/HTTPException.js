b1527f2c09550aaa719491698cd0bb7632c27f9d
emilvn <emilvnielsen@hotmail.com>
2023-09-06 17:11:07 +0200

    http exception comment

--- backend/src/errors/HTTPException.js
+++ backend/src/errors/HTTPException.js
@@ -1,4 +1,11 @@
-// custom error class for http errors //
+/**
+ * HTTPException
+ * Custom class for handling HTTP errors
+ * @class
+ * @extends Error
+ * @param {String} message error message for exception
+ * @param {Number} statusCode HTTP status code for exception
+ */
 export class HTTPException extends Error {
 	constructor(message, statusCode) {
 		super(message);

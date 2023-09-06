afe6a953d8be9ec75c3c90b2aab9ff3caf980e84
emilvn <emilvnielsen@hotmail.com>
2023-09-06 17:54:11 +0200

    added comments to getfavorites

--- backend/src/helpers/getfavorites.js
+++ backend/src/helpers/getfavorites.js
@@ -7,6 +7,7 @@
  * gets array of artists whose id's are in favoriteIds
  * @param {string[]} favoriteIDs array of artist id's
  * @param {Artist[]} artists array of Artist objects
+ * @returns {Artist[]} array of Artist objects
  */
 export function getFavorites(favoriteIDs, artists){
 	const favorites = [];

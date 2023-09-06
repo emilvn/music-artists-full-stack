78f5cbec2cc0c27cc81618b0ea33008e3a5a61d6
emilvn <emilvnielsen@hotmail.com>
2023-09-06 17:52:14 +0200

    added comments to getfavorites

--- backend/src/helpers/getfavorites.js
+++ backend/src/helpers/getfavorites.js
@@ -1,6 +1,16 @@
-export function getFavorites(favoriteIDs, artists) {
+/**
+ * @typedef {import(Artist)} Artist
+ */
+
+/**
+ * getFavorites
+ * gets array of artists whose id's are in favoriteIds
+ * @param {string[]} favoriteIDs array of artist id's
+ * @param {Artist[]} artists array of Artist objects
+ */
+export function getFavorites(favoriteIDs, artists){
 	const favorites = [];
-	for (const id of favoriteIDs) {
+	for(const id of favoriteIDs){
 		const favoriteArtist = artists.find(artist => artist.id === id);
 		favorites.push(favoriteArtist);
 	}
---
+++ backend/src/types/types.js
@@ -0,0 +1,13 @@
+export /**
+ * @typedef {Object} Artist
+ * @property {string} id - unique id of the artist
+ * @property {string} name - name of the artist
+ * @property {string} birthdate - birthdate of the artist in format "YYYY-MM-DD"
+ * @property {string} activeSince - date when the artist became active in format "YYYY-MM-DD"
+ * @property {string[]} genres - array of genres associated with the artist
+ * @property {string[]} labels - array of record labels associated with the artist
+ * @property {string} website - website URL of the artist
+ * @property {string} image - URL of an image of the artist
+ * @property {string[]} roles - array of roles held by the artist
+ * @property {string} shortDescription - short description of the artist
+ */
